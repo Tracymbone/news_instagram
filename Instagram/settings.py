2da505cf2acfee53aac960f540d5d3d9ef51be4a
moringa <tressy.mbone@student.moringaschool.com>
2022-03-20 20:29:14 +0300

    deploy to heroku

--- Instagram/settings.py
+++ Instagram/settings.py
@@ -51,7 +51,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9nhn4toeoz%g&bu%%@6r6f+4dyb3$+&koeqas3$d^awy04!-!l'
+SECRET_KEY = 'SECRET_KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
